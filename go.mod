94f7ba3e0603248cdae3f3180949cd3fdf2fe021
dillonthompson <dthompson@virtru.com>
2022-01-28 15:39:11 -0600

    updated packages and adjusted code accordingly

--- go.mod
+++ go.mod
@@ -1,11 +1,27 @@
-module github.com/virtru-corp/oteltracer
+module github.com/virttu-corp/oteltracer
 
-go 1.15
+go 1.17
 
 require (
-	go.opentelemetry.io/otel v0.17.0
-	go.opentelemetry.io/otel/exporters/otlp v0.17.0
-	go.opentelemetry.io/otel/exporters/stdout v0.17.0
-	go.opentelemetry.io/otel/sdk v0.17.0
-	google.golang.org/grpc v1.36.0
+	go.opentelemetry.io/otel v1.3.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.3.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.3.0
+	go.opentelemetry.io/otel/sdk v1.3.0
+	google.golang.org/grpc v1.44.0
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
+	github.com/go-logr/logr v1.2.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.3.0 // indirect
+	go.opentelemetry.io/otel/trace v1.3.0 // indirect
+	go.opentelemetry.io/proto/otlp v0.12.0 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- tracer.go
+++ tracer.go
@@ -6,72 +6,58 @@ import (
 	"os"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpgrpc"
-	"go.opentelemetry.io/otel/exporters/stdout"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/propagation"
-	exporter "go.opentelemetry.io/otel/sdk/export/trace"
+
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/semconv"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 
 	"google.golang.org/grpc"
 )
 
 var otelGRPCCollector = os.Getenv("OTLP_COLLECTOR_ENDPOINT") //ex. localhost:4345
 
-var stdoutTrace = false
-
 func InitTracer(serviceName string) func() {
-	var exporter exporter.SpanExporter
 	var shutdownFunc func()
 	var err error
-	if stdoutTrace {
-		// Create stdout exporter to be able to retrieve
-		// the collected spans.
-		log.Println("Starting OpenTelemetry tracer: stdout")
-		exporter, err = stdout.NewExporter()
+
+	log.Printf("Starting OpenTelemetry tracer: otlp, configured with endpoint: %s", otelGRPCCollector)
+	otlpTracerCtx := context.Background()
+
+	// If the OpenTelemetry Collector is running on a local cluster (minikube or
+	// microk8s), it should be accessible through the NodePort service at the
+	// `localhost:30080` endpoint. Otherwise, replace `localhost` with the
+	// endpoint of your cluster. If you run the app inside k8s, then you can
+	// probably connect directly to the service through dns
+	client := otlptracegrpc.NewClient(
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint(otelGRPCCollector),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()), // useful for testing
+	)
+	if err != nil {
+		log.Fatalf("failed to create otlp driver: %v", err)
+	}
+	exporter, err := otlptrace.New(otlpTracerCtx, client)
+	if err != nil {
+		log.Fatalf("failed to create exporter: %v", err)
+	}
+	// Return a shutdown func the caller can use to dispose tracer connection
+	shutdownFunc = func() {
+		log.Printf("Shutting down otlp tracer")
+		err := exporter.Shutdown(otlpTracerCtx)
 		if err != nil {
-			log.Fatal(err)
-		}
-
-		shutdownFunc = func() {
-			log.Printf("Shutting down stdout tracer")
-		}
-	} else {
-
-		log.Printf("Starting OpenTelemetry tracer: otlp, configured with endpoint: %s", otelGRPCCollector)
-		otlpTracerCtx := context.Background()
-
-		// If the OpenTelemetry Collector is running on a local cluster (minikube or
-		// microk8s), it should be accessible through the NodePort service at the
-		// `localhost:30080` endpoint. Otherwise, replace `localhost` with the
-		// endpoint of your cluster. If you run the app inside k8s, then you can
-		// probably connect directly to the service through dns
-		driver := otlpgrpc.NewDriver(
-			otlpgrpc.WithInsecure(),
-			otlpgrpc.WithEndpoint(otelGRPCCollector),
-			otlpgrpc.WithDialOption(grpc.WithBlock()), // useful for testing
-		)
-		exporter, err = otlp.NewExporter(otlpTracerCtx, driver)
-		if err != nil {
-			log.Fatalf("failed to create exporter: %v", err)
-		}
-		// Return a shutdown func the caller can use to dispose tracer connection
-		shutdownFunc = func() {
-			log.Printf("Shutting down otlp tracer")
-			err := exporter.Shutdown(otlpTracerCtx)
-			if err != nil {
-				log.Fatalf("failed to stop exporter: %v", err)
-			}
+			log.Fatalf("failed to stop exporter: %v", err)
 		}
 	}
 
 	// For the demonstration, use sdktrace.AlwaysSample sampler to sample all traces.
 	// 	In a production application, use sdktrace.ProbabilitySampler with a desired probability.
-	tp := sdktrace.NewTracerProvider(sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 		sdktrace.WithSyncer(exporter),
-		sdktrace.WithResource(resource.NewWithAttributes(semconv.ServiceNameKey.String(serviceName))))
+		sdktrace.WithResource(resource.NewWithAttributes(semconv.SchemaURL, semconv.ServiceNameKey.String(serviceName))))
 	otel.SetTracerProvider(tp)
 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
 
